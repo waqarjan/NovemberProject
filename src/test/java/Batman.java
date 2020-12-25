b29aeeb106aef34dac10b57b5f11111ef45de22d
WAQAR JAN <wakarjan@hotmail.com>
2020-12-25 03:12:40 -0500

    Batman has bat bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -5,6 +5,7 @@ public class Batman {
         System.out.println("I will catch you joker");
         //Yes he will catch him
         //Batman has cool Bat mobile
+        //Batman has cool Bat motorcycle
 
     }
 }
